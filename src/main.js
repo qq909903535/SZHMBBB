78b1d73106453aa3d3eb0ac89873e00ff4103b77
lijiaming <909903535@qq.com>
2018-07-26 09:03:39 +0800

    完成了首页路由和首页的基本结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
+//引入vue模块
 import Vue from 'vue'
+//引入app模板根节点
 import App from './App.vue'
+//引入index组件
+import index from './components/index.vue'
+//引入路由模块
+import VueRouter from 'vue-router'
 
+//使用路由中间件
+Vue.use(VueRouter)
+
+//定义路由规则
+const router = new VueRouter({
+  routes :[
+    { path: '/index', component: index }
+  ],
+})
+
+//引入css 
+import './assets/statics/site/css/style.css';
+
+
+//关闭vue窗口错误提示
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
